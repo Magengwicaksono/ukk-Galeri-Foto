15992ce453bc966b7cc0ded07997513dcbd2016f
Magengwicaksono <warior76dwlseal@gmail.com>
2025-05-23 07:31:17 +0700

    Update album.php

--- ukk_gallery2/admin/album.php
+++ ukk_gallery2/admin/album.php
@@ -125,7 +125,7 @@
     <?php } ?>
     </table>
             <footer class="d-flex justify-content-center border-top mt-3 bg-light fixed-bottom">
-                <p>&copy; UKK RPL 2025 | Ibnu Ubaidillah</p>
+                <p>&copy; UKK RPL 2025 | Ageng Wicaksono</p>
             </footer>
 
 
