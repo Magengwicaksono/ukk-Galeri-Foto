37336dfb180f9e62c9eb204d5fcab0f91c18571a
Magengwicaksono <warior76dwlseal@gmail.com>
2025-05-23 07:29:44 +0700

    Update register.php

--- ukk_gallery2/register.php
+++ ukk_gallery2/register.php
@@ -56,7 +56,7 @@
 </div>
 
 <footer class="d-flex justify-content-center border-top mt-3 bg-light fixed-bottom">
-	<p>&copy ; UKK RPL 2025 | Ibnu Ubaidillah</p>
+	<p>&copy ; UKK RPL 2025 | Ageng Wicaksono</p>
 </footer>
 
 
